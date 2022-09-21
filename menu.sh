6499c52f0f204c149d63404e1c2fe73d14efce41
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2022-09-22 01:57:18 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,8 +9,24 @@ PURPLE="\e[035;1m"
 CYAN="\e[036;1m"
 LIGHT="\e[037;1m"
 NC="\e[0m"
-echo -e " ${YELLOW} -----=[${NC} ${RED}PANEL MENU BY SSHINJECTOR.NET${NC} ${YELLOW}]=----- ${NC}"
-echo -e ""
+
+# NGINX
+BCD1=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $nginx == "running" ]]; then
+    status_nginx="${GREEN}ON${NC}"
+else
+    status_nginx="${RED}OFF${NC}"
+fi
+
+# XRAY
+BCD2=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $xray == "running" ]]; then
+    status_xray="${GREEN}ON${NC}"
+else
+    status_xray="${RED}OFF${NC}"
+fi
+
+echo -e " ${YELLOW} ----------=[${NC} ${RED}PREMIUM PANEL MENU${NC} ${YELLOW}]=---------- ${NC}"
 DM=$(cat /etc/xray/domain.conf)
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
@@ -28,25 +44,27 @@ echo -e "          ${GREEN} TIMEZONE :${NC}${LIGHT} ${WKT} ${NC}"
 echo -e "          ${GREEN} DAY      :${NC}${LIGHT} ${HARI} ${NC}"
 echo -e "          ${GREEN} DATE     :${NC}${LIGHT} ${TGL} ${NC}"
 echo -e "          ${GREEN} TIME     :${NC}${LIGHT} ${JAM} ${NC}"
-echo -e " ${YELLOW} --------------------------------------------- ${NC}"
-echo -e "    ${GREEN} 1${NC}${LIGHT})${NC} ${LIGHT}PANEL SHADOWSOCKS${NC}"
-echo -e "    ${GREEN} 2${NC}${LIGHT})${NC} ${LIGHT}PANEL TROJAN${NC}"
-echo -e "    ${GREEN} 3${NC}${LIGHT})${NC} ${LIGHT}PANEL VLESS${NC}"
-echo -e "    ${GREEN} 4${NC}${LIGHT})${NC} ${LIGHT}PANEL VMESS${NC}"
-echo -e "    ${GREEN} 5${NC}${LIGHT})${NC} ${LIGHT}ADD NEW HOST${NC}"
-echo -e "    ${GREEN} 6${NC}${LIGHT})${NC} ${LIGHT}RENEW CERT XRAY${NC}"
-echo -e "   ${GREEN}  7${NC}${LIGHT})${NC} ${LIGHT}CHECK USAGE RAM${NC}"
-echo -e "   ${GREEN}  8${NC}${LIGHT})${NC} ${LIGHT}CHECK BANDWIDTH VPS${NC}"
-echo -e "   ${GREEN}  9${NC}${LIGHT})${NC} ${LIGHT}SPEEDTEST VPS${NC}"
-echo -e "   ${GREEN} 10${NC}${LIGHT})${NC} ${LIGHT}INFO RUNNING SYSTEM${NC}" 
-echo -e "   ${GREEN} 11${NC}${LIGHT})${NC} ${LIGHT}INFO SCRIPT${NC}"
-echo -e "   ${GREEN} 12${NC}${LIGHT})${NC} ${LIGHT}REBOOT VPS${NC}"
-echo -e " ${YELLOW} --------------------------------------------- ${NC}"
+echo -e " ${YELLOW} -------------------------------------------- ${NC}"
+echo -e " ${YELLOW} │         ${LIGHT}NGINX =${NC} ${BCD1}      ${LIGHT}XRAY =${NC} ${BCD2}        │${NC} "
+echo -e " ${YELLOW} -------------------------------------------- ${NC}"
+echo -e "    ${GREEN} 1${NC}${LIGHT}) PANEL SHADOWSOCKS${NC}"
+echo -e "    ${GREEN} 2${NC}${LIGHT}) PANEL TROJAN${NC}"
+echo -e "    ${GREEN} 3${NC}${LIGHT}) PANEL VLESS${NC}"
+echo -e "    ${GREEN} 4${NC}${LIGHT}) PANEL VMESS${NC}"
+echo -e "    ${GREEN} 5${NC}${LIGHT}) ADD NEW HOST${NC}"
+echo -e "    ${GREEN} 6${NC}${LIGHT}) RENEW CERT XRAY${NC}"
+echo -e "   ${GREEN}  7${NC}${LIGHT}) CHECK USAGE RAM${NC}"
+echo -e "   ${GREEN}  8${NC}${LIGHT}) CHECK BANDWIDTH VPS${NC}"
+echo -e "   ${GREEN}  9${NC}${LIGHT}) SPEEDTEST VPS${NC}"
+echo -e "   ${GREEN} 10${NC}${LIGHT}) INFO RUNNING SYSTEM${NC}" 
+echo -e "   ${GREEN} 11${NC}${LIGHT}) INFO SCRIPT${NC}"
+echo -e "   ${GREEN} 12${NC}${LIGHT}) REBOOT VPS${NC}"
+echo -e " ${YELLOW} -------------------------------------------- ${NC}"
 echo -e "       ${GREEN} x${NC}${LIGHT})${NC} ${LIGHT}EXIT${NC}"
-echo -e " ${YELLOW} --------------------------------------------- ${NC}"
+echo -e " ${YELLOW} -------------------------------------------- ${NC}"
 echo -e ""
 echo -e "    ${LIGHT} Select From Options ${NC}"
-read -p "     [1-18 or type x to exit the menu] : " menuu
+read -p "     [1-12 or type x to exit the menu] : " menuu
 echo -e ""
 case $menuu in
 1)
