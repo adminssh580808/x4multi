eee03065c10064a3a5c10b0d9dea19e2502e30b6
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2022-09-21 17:56:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # ===================================
 
 function import_string() {
-    export SCRIPT_URL='https://raw.githubusercontent.com/adminssh580808/x4i/main'
+    export SCRIPT_URL='https://raw.githubusercontent.com/adminssh580808/JKW/main'
     export RED="\033[0;31m"
     export GREEN="\033[0;32m"
     export YELLOW="\033[0;33m"
