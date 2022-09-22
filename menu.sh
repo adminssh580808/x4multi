3c88bb1f319b088bb23007ba22283a258f4a0405
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2022-09-22 21:48:47 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,10 +9,9 @@ PURPLE="\e[035;1m"
 CYAN="\e[036;1m"
 LIGHT="\e[037;1m"
 NC="\e[0m"
-
 echo -e ""
 echo -e ""
-echo -e "  ${YELLOW}------=[${NC} ${RED}AUTO SCRIPT PREMIUM BY SSHINJECTOR.NET${NC} ${YELLOW}]=------ ${NC}"
+echo -e "  ${YELLOW}----------=[${NC} ${RED}PREMIUM PANEL MENU${NC} ${YELLOW}]=---------- ${NC}"
 DOMAIN=$(cat /etc/xray/domain.conf)
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
@@ -21,43 +20,35 @@ IP=$(curl -s ipinfo.io/ip )
 JAM=$(date +"%T")
 HARI=$(date +"%A")
 TGL=$(date +"%d-%B-%Y")
+echo -e "      ${GREEN} ISP NAME${NC}${LIGHT} : ${ISP} ${NC}"
+echo -e "      ${GREEN} DOMAIN${NC}${LIGHT}   : ${DOMAIN} ${NC}"
+echo -e "      ${GREEN} IP VPS${NC}${LIGHT}   : ${IP} ${NC}"
+echo -e "      ${GREEN} DAY${NC}${LIGHT}      : ${HARI} ${NC}"
+echo -e "      ${GREEN} DATE${NC}${LIGHT}     : ${TGL} ${NC}"
+echo -e "      ${GREEN} TIME${NC}${LIGHT}     : ${JAM} ${NC}"
+echo -e "      ${GREEN} CITY${NC}${LIGHT}     : ${CITY} ${NC}"
+echo -e "      ${GREEN} TIMEZONE${NC}${LIGHT} : ${WKT} ${NC}"
+echo -e "  ${YELLOW}-------------------------------------------- ${NC}"
+echo -e "  ${LIGHT}  CRON =${NC} BCD1      ${LIGHT}NGINX =${NC} $BCD2      ${LIGHT}XRAY =${NC} $BCD3"
+echo -e "  ${YELLOW}-------------------------------------------- ${NC}"
+echo -e "  ${YELLOW}-------------=[${NC} ${RED}XRAY SERVICE${NC} ${YELLOW}]=------------- ${NC}"
+echo -e "    ${GREEN} 1${NC}${LIGHT}). PANEL SHADOWSOCKS${NC}"
+echo -e "    ${GREEN} 2${NC}${LIGHT}). PANEL TROJAN${NC}"
+echo -e "    ${GREEN} 3${NC}${LIGHT}). PANEL VLESS${NC}"
+echo -e "    ${GREEN} 4${NC}${LIGHT}). PANEL VMESS${NC}"
 echo -e ""
-echo -e "          ${GREEN} ISP NAME :${NC}${LIGHT} ${ISP} ${NC}"
-echo -e "          ${GREEN} DOMAIN   :${NC}${LIGHT} ${DOMAIN} ${NC}"
-echo -e "          ${GREEN} IP VPS   :${NC}${LIGHT} ${IP} ${NC}"
-echo -e "          ${GREEN} DAY      :${NC}${LIGHT} ${HARI} ${NC}"
-echo -e "          ${GREEN} DATE     :${NC}${LIGHT} ${TGL} ${NC}"
-echo -e "          ${GREEN} TIME     :${NC}${LIGHT} ${JAM} ${NC}"
-echo -e "          ${GREEN} CITY     :${NC}${LIGHT} ${CITY} ${NC}"
-echo -e "          ${GREEN} TIMEZONE :${NC}${LIGHT} ${WKT} ${NC}"
-echo -e ""
-echo -e " ${YELLOW} -------------------=[${NC} ${RED}XRAY SERVICE${NC} ${YELLOW}]=------------------- ${NC}"
-echo -e "    ${GREEN} 1${NC}${LIGHT})${NC} ${LIGHT}PANEL SHADOWSOCKS${NC}"
-echo -e "    ${GREEN} 2${NC}${LIGHT})${NC} ${LIGHT}PANEL TROJAN${NC}"
-echo -e "    ${GREEN} 3${NC}${LIGHT})${NC} ${LIGHT}PANEL VLESS${NC}"
-echo -e "    ${GREEN} 4${NC}${LIGHT})${NC} ${LIGHT}PANEL VMESS${NC}"
-echo -e ""
-echo -e " ${YELLOW} -------------------=[${NC} ${RED}OPTIONS MENU${NC} ${YELLOW}]=------------------- ${NC}"
-echo -e "    ${GREEN} 5${NC}${LIGHT})${NC} ${LIGHT}ADD SUBDOMAIN HOST FOR VPS${NC}"
-echo -e "    ${GREEN} 6${NC}${LIGHT})${NC} ${LIGHT}RENEW CERTIFICATE XRAY${NC}"
-echo -e "    ${GREEN} 7${NC}${LIGHT})${NC} ${LIGHT}AUTOBACKUP DATA VPS${NC}"
-echo -e "    ${GREEN} 8${NC}${LIGHT})${NC} ${LIGHT}BACKUP DATA VPS${NC}"
-echo -e "    ${GREEN} 9${NC}${LIGHT})${NC} ${LIGHT}RESTORE DATA VPS${NC}"
-echo -e "   ${GREEN} 10${NC}${LIGHT})${NC} ${LIGHT}LIMIT BANDWITH SPEED SERVER${NC}"
-echo -e "   ${GREEN} 11${NC}${LIGHT})${NC} ${LIGHT}CHECK USAGE OF VPS RAM${NC}"
-echo -e "   ${GREEN} 12${NC}${LIGHT})${NC} ${LIGHT}REBOOT VPS${NC}"
-echo -e "   ${GREEN} 13${NC}${LIGHT})${NC} ${LIGHT}SPEEDTEST VPS${NC}"
-echo -e "   ${GREEN} 14${NC}${LIGHT})${NC} ${LIGHT}INFORMATION DISPLAY SYSTEM${NC}" 
-echo -e "   ${GREEN} 15${NC}${LIGHT})${NC} ${LIGHT}INFORMATION SCRIPT${NC}"
-echo -e "   ${GREEN} 16${NC}${LIGHT})${NC} ${LIGHT}CLEAR LOG VPS${NC}"
-echo -e "   ${GREEN} 17${NC}${LIGHT})${NC} ${LIGHT}CEK BANDWIDTH VPS${NC}"
-echo -e "   ${GREEN} 18${NC}${LIGHT})${NC} ${LIGHT}CEK RUNNING SERVICE${NC}"
-echo -e " ${YELLOW} -------------------------------------------------------- ${NC}"
-echo -e "       ${GREEN} x${NC}${LIGHT})${NC} ${LIGHT}EXIT${NC}"
-echo -e " ${YELLOW} -------------------------------------------------------- ${NC}"
+echo -e "  ${YELLOW}-------------=[${NC} ${RED}OPTIONS MENU${NC} ${YELLOW}]=------------- ${NC}"
+echo -e "    ${GREEN} 5${NC}${LIGHT}). ADD NEW SUBDOMAIN${NC}"
+echo -e "    ${GREEN} 6${NC}${LIGHT}). RENEW CERT XRAY${NC}"
+echo -e "    ${GREEN} 7${NC}${LIGHT}). SPEEDTEST SERVER${NC}"
+echo -e "    ${GREEN} 8${NC}${LIGHT}). CHECK BANDWIDTH VPS${NC}"
+echo -e "    ${GREEN} 9${NC}${LIGHT}). CHECK RUNNING SERVICE${NC}"
+echo -e "   ${GREEN} 10${NC}${LIGHT}). INFORMATION SCRIPT${NC}"
+echo -e "   ${GREEN} 11${NC}${LIGHT}). REBOOT VPS${NC}"
+echo -e "  ${YELLOW}-------------------------------------------- ${NC}"
 echo -e ""
 echo -e "    ${LIGHT} Select From Options ${NC}"
-read -p "     [1-18 or type x to exit the menu] : " menuu
+read -p "     [1-11 or type x to exit the menu] : " menuu
 echo -e ""
 case $menuu in
 1)
@@ -93,16 +84,52 @@ clear
 menu
 ;;
 5)
+#!/bin/bash
 clear
-addhost
+
+read -rp "Input ur domain : " -e domain
+
+echo "$domain" >/etc/xray/domain.conf
+domain=$(cat /etc/xray/domain.conf)
+
+# Menghentikan Port 443 & 80 jika berjalan
+lsof -t -i tcp:80 -s tcp:listen | xargs kill >/dev/null 2>&1
+lsof -t -i tcp:443 -s tcp:listen | xargs kill >/dev/null 2>&1
+sleep 0.5
+systemctl stop nginx
+cd
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    
+systemctl daemon-reload
+systemctl restart nginx
+systemctl restart xray@tls
+systemctl restart xray@nontls
 echo ""
 read -p "Click enter to return to the main menu..."
 clear
 menu
 ;;
 6)
-clear
-xray-cert
+#!/bin/bash
+
+domain=$(cat /etc/xray/domain.conf)
+
+# Menghentikan Port 443 & 80 jika berjalan
+lsof -t -i tcp:80 -s tcp:listen | xargs kill >/dev/null 2>&1
+lsof -t -i tcp:443 -s tcp:listen | xargs kill >/dev/null 2>&1
+sleep 0.5
+systemctl stop nginx
+cd
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    
+systemctl daemon-reload
+systemctl restart nginx
+systemctl restart xray@tls
+systemctl restart xray@nontls
 echo ""
 read -p "Click enter to return to the main menu..."
 clear
